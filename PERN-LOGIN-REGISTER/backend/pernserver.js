2b3419add3c740fb92452954a242153d7be78f5b
Sarah Robertson <sarah.robertson121@gmail.com>
2026-03-29 16:07:54 +0100

    registering routes

--- PERN-LOGIN-REGISTER/backend/pernserver.js
+++ PERN-LOGIN-REGISTER/backend/pernserver.js
@@ -1,17 +1,23 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: procecss.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 

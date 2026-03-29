acd37f1baa491e666776e8c7c9128576c64737f3
Sarah Robertson <sarah.robertson121@gmail.com>
2026-03-29 14:42:09 +0100

    routes correction

--- PERN-LOGIN-REGISTER/backend/routes/auth.js
+++ PERN-LOGIN-REGISTER/backend/routes/auth.js
@@ -48,8 +48,9 @@ router.post('/register', async (req, res) => {
 })
 
 // Login
-router.post('/login', async (req. res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     return res.status(400).json({ message: 'Please provide all required fields'})
   }
